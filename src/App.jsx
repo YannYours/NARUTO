22ec08400958bc346b2441ab49f8eb3d2a6c951c
YannYours <159908675+YannYours@users.noreply.github.com>
2025-04-22 15:31:57 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Accueil from "./pages/Accueil";
 import Battailles from "./pages/Batailles";
 import Personnages from "./pages/Personnages";
+import APropos from "./pages/APropos";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Accueil/>} />
-        <Route path="/batailles" element={<Battailles/>} />
-        <Route path="/personnages" element={<Personnages/>} />
-
+        <Route path="/" element={<Accueil />} />
+        <Route path="/batailles" element={<Battailles />} />
+        <Route path="/personnages" element={<Personnages />} />
+        <Route path="/apropos" element={<APropos />} />
       </Routes>
     </BrowserRouter>
 
